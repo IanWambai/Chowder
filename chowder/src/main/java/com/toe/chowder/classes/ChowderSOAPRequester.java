dae1b8e43acae9d821011183b33bf7d3eceefc61
Ian Wambai <ianwambai@gmail.com>
2017-09-04 10:40:22 +0300

    HTTP Scheme Registry
    Register http
    
    Signed-off-by: Ian Wambai <ianwambai@gmail.com>

--- chowder/src/main/java/com/toe/chowder/classes/ChowderSOAPRequester.java
+++ chowder/src/main/java/com/toe/chowder/classes/ChowderSOAPRequester.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import com.alexgilleran.icesoap.request.impl.ApacheSOAPRequester;
 import com.toe.chowder.utils.ChowderUtils;
 
+import org.apache.http.conn.scheme.PlainSocketFactory;
 import org.apache.http.conn.scheme.Scheme;
 import org.apache.http.conn.scheme.SchemeRegistry;
 
@@ -23,6 +24,7 @@ public class ChowderSOAPRequester extends ApacheSOAPRequester {
     @Override
     protected SchemeRegistry getSchemeRegistry() {
         SchemeRegistry schemeRegistry = new SchemeRegistry();
+        schemeRegistry.register(new Scheme(HTTP_NAME, PlainSocketFactory.getSocketFactory(), DEFAULT_HTTP_PORT));
         schemeRegistry.register(new Scheme(HTTPS_NAME, ChowderUtils.hostnameVerification(context), DEFAULT_HTTPS_PORT));
         return schemeRegistry;
     }
